0de765b0502a3eaa79ffa82c3ed5e0be8035b64e
程丰平 <chenfp@jsrxjt.com>
2021-05-07 19:37:08 +0800

    修改同步测试的代码

--- interview-15/src/main/java/org/itstack/interview/AtomicityTest.java
+++ interview-15/src/main/java/org/itstack/interview/AtomicityTest.java
@@ -1,20 +1,33 @@
 package org.itstack.interview;
 
+import java.util.concurrent.CountDownLatch;
+
 public class AtomicityTest {
 
     private static volatile int counter = 0;
 
+    // private  static final
+
     public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(10);
         for (int i = 0; i < 10; i++) {
             Thread thread = new Thread(() -> {
+
+                System.out.println("cuurent thread id:" + Thread.currentThread().getName());
                 for (int i1 = 0; i1 < 10000; i1++) {
+
                     add();
+                   // addNoSync();
                 }
+                countDownLatch.countDown();
+                System.out.println("count down count:" + countDownLatch.getCount());
             });
+
             thread.start();
         }
         // 等10个线程运行完毕
-        Thread.sleep(1000);
+        //Thread.sleep(1000);
+        countDownLatch.await();
         System.out.println(counter);
     }
 
@@ -22,6 +35,10 @@ public class AtomicityTest {
         synchronized (AtomicityTest.class) {
             counter++;
         }
+    }
+    public static void addNoSync() {
+        counter++;
+
     }
 
 }
